3874a0534195dc333960b643ce597415172a5d7e
JoanNgethe <92122990+HubiscusTea0@users.noreply.github.com>
2021-10-08 19:24:45 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,29 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Australia has a summer Christmas");
+  }
+}
+/*this is a comment*/
+//This is another comment
+/* Java Variable Types
+You have 5 types of variables in Java
+Boolean: Only 2 states True/False 
+Integers: Int, always WHOLE numbers
+Strings: Text, ex "Hello"
+Floats: Decimals, ex; 19.99 or -19.99
+Char: Single Characters, ex "a" "B", WRITE A CODE THAT GIVE YOU HOW MANY CHARACTERS ARE IN "Pneumonoultramicroscopicsilicovolcanoconiosis"
+
+
+In math x=4 means 4=x
+In programing x=4 ( 4 is assigned to x)
+
+x="patty"
+b= "Tomato"
+c= "Cheese"
+d= "Bread"
+e=" Lettuce"
+f= "Ketchup"
+g= "Mayo"
+
+So the template is: type variable = value; 
+*/
